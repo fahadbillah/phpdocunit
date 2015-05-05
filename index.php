3be1b5ef90d9d124d528b066bd3de55716762798
Fahad Billah <fahadbillah@yahoo.com>
2015-05-06 05:36:41 +0600

    first commit, phpdoc is working

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+	/**
+ * undocumented class
+ *
+ * @package default
+ * @author fahad
+ **/
+	class User{
+
+		/**
+	 * Index
+	 *
+	 * @return void
+	 * @author fahad
+	 **/
+		public function index()
+		{
+		}
+
+	} 
+	// END class 
+
+	?>
